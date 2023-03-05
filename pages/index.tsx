d2fc2d4091d2bddeca2349445e03923931e6de84
Saad Ahmed Qureshi <101616957+Qsaaad1@users.noreply.github.com>
2023-03-05 18:40:32 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Education from "@/components/Education";
+import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Saad's Portfolio</title>
+      </Head>
+
+      {/* Header */}
+
+      <Header />
+
+      {/* Hero */}
+
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
+
+      {/* Aboot */}
+
+      <section id="about" className="snap-center">
+        <About />
+      </section>
+
+      {/* Education */}
+
+      <section id="education" className="snap-center">
+        <Education />
+      </section>
+
+      {/* Skills */}
+
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
+
+      {/* Projects */}
+
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
+
+      {/* Contact ME */}
+    </div>
+  );
+}
